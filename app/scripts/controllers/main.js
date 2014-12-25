d0dea425a853de60d46e417612c537eebae42473
atle weibell <atle@weibell.no>
2014-12-25 22:32:43 +0100

    Adding register car functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,27 @@
 'use strict';
 
 angular.module('biloggApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope, $firebase, firebaseRef) {
+
+        // syncData('cars').$bind($scope, 'cars');
+
+        var ref = firebaseRef('cars');
+        $scope.cars = $firebase(ref);
+
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+
+        $scope.addNewCar = function () {
+            console.log("Adding new car");
+            $scope.editNewCar = true;
+        };
+
+        $scope.createCar = function () {
+            console.log("Adding car to collection");
+            $scope.cars.$add( {'regNo': $scope.newCar.regNo,  'make': $scope.newCar.make,  'model': $scope.newCar.model} );
+            $scope.editNewCar = false;
+        };
+    });
